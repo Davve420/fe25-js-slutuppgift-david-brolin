2f35011c70d8f7f6e33bc81cbe7801940606c78f
Davve420 <davidbrolin04@gmail.com>
2025-12-11 11:10:18 +0100

    first commit

---
+++ lists.js
@@ -0,0 +1,21 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjYyYmMzODhiNjk3MzdmZTE2MmQwZjZhNjE5ODk4YyIsIm5iZiI6MTc2NTM3MTM1NS40MTYsInN1YiI6IjY5Mzk2ZGRiM2JiOTA5NGFhZTNjZjFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._AY0IhBxbHM0fgy0jMJx7z4YVZys1K1jgB-TYp31D8I'
+    }
+};
+
+
+
+fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => { 
+        console.log(res);
+    })
+    .catch(err => console.error(err));
+
+    console.log("----------")
+
+
+    // console.log(res.results);
